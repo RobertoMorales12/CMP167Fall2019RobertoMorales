6df8752e3031bd9f4a8106c2e50b2c6c9a7277b3
RobertoMorales12 <54601017+RobertoMorales12@users.noreply.github.com>
2019-10-03 16:42:28 -0400

    Add files via upload

--- Loops.java
+++ Loops.java
@@ -1,21 +1,9 @@
-import java.util.Scanner;
+
 public class Loops {
-    
-    public static void main(String[] args) {
-        int userNum;
-        double i = 1;
-        Scanner scnr = new Scanner(System.in);
-        
-        System.out.println("Enter a number for some magic: ");
-        userNum = scnr.nextInt();
-        
-        for(i = 1; i <= userNum; ++i) {
-            System.out.println(i);
-        }
-        while(i <= userNum) {
-            System.out.println(Math.pow(2, i));
-            ++i;
-        }
-    }
-    
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
