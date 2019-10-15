97562d08a3c4617dad8bc6af52cde8deb85f7931
RobertoMorales12 <54601017+RobertoMorales12@users.noreply.github.com>
2019-10-15 16:32:54 -0400

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,15 @@
+package week6;
+
+public class Animal {
+	public int numLegs;
+	
+	private int age;
+	
+	public void bark() {
+		System.out.println("woof");
+	}
+	public void run() {
+		System.out.println("I run fast");
+	}
+
+}
